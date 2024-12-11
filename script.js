c2c0b9a534993d9f6955d8e2a0703c40ef4cf371
giorgosn7 <13-13908@saeinstitute.edu>
2024-12-11 21:03:19 +0200

    this is my first git

---
+++ script.js
@@ -0,0 +1,25 @@
+const arr = [1, 2, 4, 6, 10, 20] ;
+
+const arr2 = arr.slice(0, 4 ) // firat number is the begging, second number is the end of the value is not included
+/console.log('this is arr:', arr);
+console.log('this is arr2(0, 4):', arr2);
+
+//------------splice--------//
+
+//const arrSplice = [1, 2, 4, 6, 10, 20] ;
+
+//const arrSplice2 = arrSplice2.slice(0, 4 )
+
+//console.log('this is arr:', arrSplice);
+//console.log('this is arr2(o, 4):', arrSplice2);
+
+
+//---------reverse----------//
+
+//const orininalList = [1, 2, 4, 6, 10, 20];
+//const reversedList = originalList.reverse();
+
+
+//console.log(`originalList:` , originalList);
+//console.log (`reversedList:`)
+
